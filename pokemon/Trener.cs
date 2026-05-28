e28d1a2cede0c174fbcd4266639f0b66f4c8c6d1
einghstein <103892112+einghstein@users.noreply.github.com>
2026-05-28 11:38:34 +0200

    Initialize nodes in Trainer constructor
    Initialized nodes list and added a new Node with weights in the Trainer constructor.

--- pokemon/Trener.cs
+++ pokemon/Trener.cs
@@ -1,17 +1,27 @@
-﻿
+
 public class Trainer
 {
     public string Name { get; set; }
     public int Level { get; set; }
     public List<Cichnamon> Cichnamons { get; set; }
     public Cichnamon ActiveCichnamon { get; set; }
+    public List<Node> nodes { get; set; }
 
     public Trainer(string name)
-    {
-        Name = name;
-        Level = 1;
-        Cichnamons = new List<Cichnamon>();
-    }
+{
+    Name = name;
+    Level = 1;
+    Cichnamons = new List<Cichnamon>();
+    nodes = new List<Node>();
+
+        // 1. Vytvoříme hlavní pole o velikosti 1
+        double[][] Weights = new double[2][];
+        Weights[0] = new double[3]; // Automaticky obsahuje [0.0, 0.0]
+        Weights[1] = new double[3];
+
+        nodes.Add(new Node(Weights, [0.0, 1.0, 0.0]));
+}
+
 
     public void ChooseActiveCichnamon(int index)
     {
@@ -19,6 +29,11 @@ public class Trainer
         {
             ActiveCichnamon = Cichnamons[index];
         }
+    }
+
+    public void turn(Cichnamon cichnamon)
+    {
+
     }
 
     public void AddCichnamon(Cichnamon cichnamon)
